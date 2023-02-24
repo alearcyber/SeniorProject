39addffb59374642c483895bd0037178672fa2ee
Nick Pittman <np0035@uah.edu>
2023-02-24 15:41:27 -0600

    Cleaned up queries.py

--- server/Queries.py
+++ server/Queries.py
@@ -3,70 +3,48 @@ import json
 
 
 def get_shows():
+
+    # Setup database query
     connection = sqlite3.connect("database.db")
     cursor = connection.cursor()
     query = f'SELECT performance_id, title, image FROM Performance;'
-    #print("Sending query:", query)
 
+    # Execute the query
     cursor.execute(query)
-
     rows = cursor.fetchall()
+    connection.close()
 
-
-    #make the dummy descriptions
-
+    # Make the dummy descriptions
     new_rows = []
-    for i in range(len(rows)):
-        new_row = []
-        for element in rows[i]:
-            new_row.append(element)
-        new_row.append('INSERT DESCRIPTION HERE')
-        new_rows.append(new_row)
+    for r in rows:
+        record = list(r)
+        record.append('INSERT DESCRIPTION HERE')
+        new_rows.append(record)
 
     rows = new_rows
 
-    for row in rows:
-        pass
-        #print(row)
-
-    #connection.commit()
-    connection.close()
-
-    #CONSTURCT THE JSON
-    #output = {}
+    # CONSTRUCT THE JSON
     output = []
     for i in range(len(rows)):
         row = rows[i]
-        show_dictionary = {
+        production = {
             'id': str(row[0]),
             'name': row[1],
             'imgurl': row[2],
             'description':row[3]
         }
+        output.append(production)
 
-        #output[str(i)] = show_dictionary
-        output.append(show_dictionary)
-
-    json_object = json.dumps(output, indent=4) #convert dictionary to JSON
-    #print(json_object) # print out the JSON for testing if it worked
-
-
-
-
-
-
-
+    #convert dictionary to JSON
+    json_object = json.dumps(output, indent=4)
 
     #RETURN THE JSON
     #id, name, imgurl, description are the json thingies
     #return str(json_object)
     return output
 
-
 def test():
     get_shows()
-
-
 
 if __name__ == "__main__":
     test()

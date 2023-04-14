e2ad63c3e85786f5a8a2e8eb0911d1dc0736a36a
Sydney Melvin <sm0296@uah.edu>
2023-04-14 15:15:58 -0500

    Ticket payment page

--- client/src/routes/playhouse_seats/+page.svelte
+++ client/src/routes/playhouse_seats/+page.svelte
@@ -9,9 +9,8 @@
     import Playhouse from "../../components/Playhouse.svelte"
     import { Listgroup, ListgroupItem, Button } from 'flowbite-svelte'
 	import TicketCard from "../../components/TicketCard.svelte"
-    import { SeatStore, removeSeat } from '../../stores/SeatStore.js'
+    import { SeatStore } from '../../stores/SeatStore.js'
     import Legend from "../../components/Legend.svelte"
-    import { XMark } from 'svelte-heros-v2'
 
     /**
 	* @type {{ seats: {}; tickets: Array<{}>; performance: {title: string;}; }}
@@ -57,15 +56,13 @@
             {#each mySeatStore as { id, sec, row, seat } }
                 {#if seat } 
                     <ListgroupItem class="text-base font-semibold gap-2">
-                        <div class="relative">
-                            <Button on:click={removeSeat({ id })} on:keypress={removeSeat({ id })} pill={true} class="!p-2 absolute right-0 h-7 w-7">
-                                <XMark class="text-white h-5 w-5" />
-                            </Button>
-                        </div>
                         <TicketCard section={sec} row={row} seat={seat} price={tickets[id]?.price ?? 0} />
                     </ListgroupItem>
                 {/if}
             {/each}
         </Listgroup>
+        <div>
+            <Button class="w-full h-full" size="sm" href="/payment">Buy Tickets</Button>
+        </div>
     </div>
 </div>

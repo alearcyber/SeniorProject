a4d0a45d64257268255acb07d09ded97e3267d1e
Nick Pittman <np0035@uah.edu>
2023-04-17 12:32:50 -0500

    Added season info struct

--- client/src/routes/volunteers/season/+page.svelte
+++ client/src/routes/volunteers/season/+page.svelte
@@ -2,7 +2,6 @@
     Create Season Page
     This page is where the volunteers go to create a new season.
 -->
-
 <script>
     import { Label, Input, Button, Listgroup, Checkbox } from 'flowbite-svelte'
     import { onMount } from 'svelte';
@@ -13,16 +12,17 @@
 	});
 
     //hard coded event data that will eventually be received from database
-    let list = [
-        { name: "Phantom of the Opera"
-        },
-        { name: "Hamilton"
-        },
-        { name: "Wicked"
-        },
-    ]
+    let list = [{ name: "Placeholder production" }]
 
     let org_name = "";
+
+    let season_info = {
+        org_id: '',
+        season_name: '',
+        description: '',
+        productions: [],
+    }
+    let selected = [""];
 
     async function get_production_list() {
         let email = sessionStorage.getItem("user");
@@ -38,47 +38,40 @@
     }
     
     async function create_season() {
-
+        console.log(selected);
     }
 
     
-    let selected = [""];
 </script>
-  
-<div class="mb-10 flex justify-center" >
-    <form class="flex flex-col space-y-6" style="width:800px" action='/'>
-        <!-- Title -->
-        <h3 class="text-xl font-medium text-gray-900 dark:text-white p-0">Create New Season</h3>
 
-        <!-- Season Name Field -->
-        <Label class="space-y-2">
-          <span>Season Name</span>
-          <Input type="text" name="season_name" placeholder="" required />
-        </Label>
+<div class="mb-10 flex justify-center">
+	<form class="flex flex-col space-y-6" style="width:800px" action="/">
+		<!-- Title -->
+		<h3 class="text-xl font-medium text-gray-900 dark:text-white p-0">Create New Season</h3>
 
-        <!-- Organization Field -->
-        <Label class="space-y-2">
-          <span>Organization: {org_name}</span>
-        </Label>
+		<!-- Season Name Field -->
+		<Label class="space-y-2">
+			<span>Season Name</span>
+			<Input type="text" name="season_name" bind:value={season_info.season_name} placeholder="" required />
+		</Label>
 
-        <!-- Production List -->
-        <Label class="space-y-2">
-        <span> Production List</span>
-            {#each list as item, i}
+		<!-- Organization Field -->
+		<Label class="space-y-2">
+			<span>Organization: {org_name}</span>
+		</Label>
 
-                <div class="d-flex justify-space-around">
-                    <Checkbox group={selected} value={item.name}/> {item}<br>
-                </div>
-            {/each}   
-        </Label>
+		<!-- Production List -->
+		<Label class="space-y-2">
+			<span> Production List</span>
+			{#each list as item, i}
+				<div class="d-flex justify-space-around">
+					<Checkbox group={selected} value={item.name} />
+					{item}<br />
+				</div>
+			{/each}
+		</Label>
 
-        
-        <!-- Create Season button -->
-        <Button type="submit" class="w-full1">Create Season</Button>
-    </form>
-  </div>
-  
- 
-
-
-
+		<!-- Create Season button -->
+		<Button class="w-full1" on:click={create_season}>Create Season</Button>
+	</form>
+</div>

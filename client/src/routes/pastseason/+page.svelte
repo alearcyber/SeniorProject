2becd7dae02c789a85942f8a331405bc9c5907b9
Sydney Melvin <sm0296@uah.edu>
2023-04-23 17:46:24 -0500

    polish up and finish implementing pastseason

--- client/src/routes/pastseason/+page.svelte
+++ client/src/routes/pastseason/+page.svelte
@@ -1,107 +1,143 @@
 <script>
-    import {onMount} from "svelte";
-
+    import { Button, Input, Heading } from 'flowbite-svelte'
+    import { onMount } from 'svelte'
+    import Center from '../../layouts/center.svelte' //import Center layout
 
     //check that the user is logged in
-    let email = 'tom.bombadillo@arnornet.me';
+    let email = '';
     onMount(async () => {
-        fetch_performances();
+        populate_season_pass_info();
+        let val = sessionStorage.getItem('user');
+		if (val !== null) {
+			email = val
+		}
     });
 
-
-    //grabbing data from database
-    let cards = [];
-
-    async function fetch_performances() {
-        let response = await fetch('http://127.0.0.1:5000/frontdesk_upcoming_performances', {
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({'email': email})
-        });
-        const out = await response.json();
-        cards = out.performances;
-    }
-
-    //handle when a card is selected
-    let selectedCards = []; //list of cards selected
-    const toggleSelection = (card) => {
-        if (selectedCards.includes(card)) {
-            selectedCards = selectedCards.filter((c) => c !== card);
-        } else {
-            selectedCards = [...selectedCards, card];
-        }
-    };
-
-
-    //handles confirm button
-    function confirm_selection() {
-        //check for nothing selected
-        if (selectedCards.length <= 0) {
-            alert("ERROR: you have not selected anything");
-        } else if (selectedCards.length > 1) {
-            alert("ERROR: you have to select ONLY ONE performance.");
-        } else {
-
-            alert(`You chose ${selectedCards[0].title}!`)
-            window.location.href = `/frontdesk/manager?title=${selectedCards[0].title}&id=${selectedCards[0].id}`;
-            //TODO - continue HERE. Take the 'combined' string of id's and send it to a new page
-            //will have to make that new page and make it so that page processes the payment information
-        }
-    }
-
-
-    function dummy() {
-      console.log(cards);
-    }
-
-
-
     //logical storage of the season passes in memory
-    let season_pass_info = [
-      {'id':1, 'email': 'ojhn@gmail.com', 'name': 'johntravolta', 'address': 'the road'},
-      {'id':2, 'email': 'one', 'name': 'two', 'address': 'three'},
-      {'id':3, 'email': 'asdfasdf', 'name': 'asdfasdf', 'address': 'twerf'},
-    ]
-
-
+    let season_pass_info = []
+    let season_pass_state = {}
 
     //this function populates the season apss info when the page loads
     async function populate_season_pass_info(){
-      let response = await fetch('http://127.0.0.1:5000/get_season_pass_info', {
+        let data = {}
+        try {
+            const response = await fetch('http://127.0.0.1:5000/get_season_pass_info', {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({'email': email})
+            })
+
+            data = await response.json()
+        } catch (error) {
+            console.error({ error })
+            data = {
+                season_passes: [
+                    {'id':1, 'email': 'ojhn@gmail.com', 'name': 'johntravolta', 'address': 'the road'},
+                    {'id':2, 'email': 'one', 'name': 'two', 'address': 'three'},
+                    {'id':3, 'email': 'asdfasdf', 'name': 'asdfasdf', 'address': 'twerf'},
+                ]
+            }
+        } 
+        
+        season_pass_info = data.data?.season_passes ?? data.season_passes
+
+        season_pass_info.forEach((item) => {
+            season_pass_state[item.id] = false;
+        })
+    }
+
+    //this function updates the information for the database
+    async function handle_submit(card) {
+        // call your function with the form data as arguments
+        console.log({ card })
+
+        const response = await fetch('http://127.0.0.1:5000/update_season_pass_info', {
             method: 'POST',
             headers: {
                 Accept: 'application/json',
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({'email': email})
-        });
-        const out = await response.json();
-        season_pass_info = out.season_passes;
+            body: JSON.stringify(card)
+        })
+
+        const data = await response.json()
+
+        console.log({ data, response })
     }
 
-
-    //this function updates the information for the database
-    function handle_submit(event){
-      event.preventDefault(); // prevent the default form submission behavior
-
-      // get the form input values from the event.target object
-      const formData = new FormData(event.target);
-      const name = formData.get('name');
-      const email = formData.get('email');
-      const address = formData.get('address');
-      const id = formData.get('id');
-
-      // call your function with the form data as arguments
-      alert(`the name is ${email}, ${name}, ${address}, ${id}`);
+    function handleInput(card) {
+        season_pass_state[card.id] = true
     }
 
-
-
+    function cardDisabled(card){
+      return season_pass_state[card.id] === false
+    }
 </script>
 
+<Center>
+    <Heading>Past Season Pass Info</Heading>
+</Center>
+
+<div>
+    {#each season_pass_info as card}
+        <div class="p-1 flex items-center justify-center w-100 text-center">
+            <div class="card flex flex-col max-w-6xl justify-start">
+                <div class="text-center font-bold text-lg">
+                    Season Pass
+                </div>
+                <form
+                    class="grid grid-cols-7 gap-2"
+                >
+                    <!-- svelte-ignore a11y-label-has-associated-control -->
+                    <label class="col-span-2">
+                        Email:
+                        <Input
+                            name="email"
+                            type="text" 
+                            bind:value={card.email}
+                            on:input={handleInput(card)}
+                        />
+                    </label>
+
+                    <!-- svelte-ignore a11y-label-has-associated-control -->
+                    <label class="col-span-2">
+                        Name:
+                        <Input 
+                            name="name" 
+                            type="text" 
+                            bind:value={card.name} 
+                            on:input={handleInput(card)}
+                        />
+                    </label>
+
+                    <!-- svelte-ignore a11y-label-has-associated-control -->
+                    <label class="col-span-2">
+                        Address:
+                        <Input 
+                            name="address" 
+                            type="text" 
+                            bind:value={card.address} 
+                            on:input={handleInput(card)}
+                        />
+                    </label>
+                    <div class="inline-flex items-bottom justify-start">
+                        <Button 
+                            type="submit" 
+                            disabled={cardDisabled(card)}
+                            on:click={handle_submit(card)}
+                            class="h-10 mt-6"
+                        >
+                            Save
+                        </Button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    {/each}
+</div>
 
 <style>
     .card {
@@ -109,7 +145,7 @@
         border-radius: 5px;
         padding: 10px;
         margin: 10px;
-        cursor: pointer;
+        box-shadow: inset 0 0 0 0 transparent;
     }
 
     .selected {
@@ -117,42 +153,3 @@
         border: 1px solid #1890ff;
     }
 </style>
-
-
-<h1>Past Season Pass Info</h1>
-
-
-<div>
-    {#each season_pass_info as card}
-        <div
-                class="card {selectedCards.includes(card) ? 'selected' : ''}"
-                on:click={() => toggleSelection(card)}
-        >
-            <p>Season Pass</p>
-            <form on:submit|preventDefault={handle_submit}>
-
-                <input name="id" type="text" value={card.id} hidden>
-
-                <label>
-                    Email:
-                    <input name="email" type="text" value={card.email}>
-                </label>
-
-                <label>
-                    Name:
-                    <input name="name" type="text" value={card.name}>
-                </label>
-
-                <label>
-                    Address:
-                    <input name="address" type="text" value={card.address}>
-                </label>
-                <button type="submit">Submit</button>
-            </form>
-        </div>
-    {/each}
-</div>
-
-<div>
-    <button on:click={dummy}>Click here to confirm your selection</button>
-</div>
